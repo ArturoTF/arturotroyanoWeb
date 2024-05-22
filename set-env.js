21217dcc6463e7b89f3155a295581e0824466dc3
ArturoTF <arturotroyano@gmail.com>
2024-05-22 15:01:37 +0200

    cargar variables de entorno

--- set-env.js
+++ set-env.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const dotenv = require('dotenv');
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
 
 const envFilePath = './src/environments/environment.railway.ts';
 const envFileContent = `
