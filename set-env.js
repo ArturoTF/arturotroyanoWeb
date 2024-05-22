3f75cdc85303472b4a2998005bc114d26131e589
ArturoTF <arturotroyano@gmail.com>
2024-05-22 15:07:34 +0200

    Revert "cargar variables de entorno"
    This reverts commit 21217dcc6463e7b89f3155a295581e0824466dc3.

--- set-env.js
+++ set-env.js
@@ -1,8 +1,4 @@
 const fs = require('fs');
-const dotenv = require('dotenv');
-
-// Cargar las variables de entorno desde el archivo .env
-dotenv.config();
 
 const envFilePath = './src/environments/environment.railway.ts';
 const envFileContent = `
